a222848934074532d5545586dab6ced34cce53cc
LeoSanchez89 <58442719+LeoSanchez89@users.noreply.github.com>
2020-06-18 19:04:24 -0400

    all tests passing

--- eating_cookies/eating_cookies.py
+++ eating_cookies/eating_cookies.py
@@ -2,23 +2,29 @@
 Input: an integer
 Returns: an integer
 '''
-import itertools
-def eating_cookies(n):
-    # cookies = []
-    
+# def eating_cookies(n):
+#     if n < 0:
+#         return 0
+#     elif n == 0:
+#         return 1
+#     else:
+#         return eating_cookies(n-1) + eating_cookies(n-2) + eating_cookies(n-3)
+
+def eating_cookies(n, cache=None):
     if n < 0:
         return 0
-    elif n == 0:
+    if n == 0:
         return 1
-    else:
-        # for i in range(n-1):
-        #     cookies.append(i)
-        # # print(list(itertools.permutations(cookies)))
-        # return len(list(itertools.permutations(cookies)))
-        return eating_cookies(n-1) + eating_cookies(n-2) + eating_cookies(n-3)
+    if cache is None:
+        cache = {}
+    if n in cache:
+        return cache[n]
+    
+    cache[n] = eating_cookies(n-1, cache) + eating_cookies(n-2, cache) + eating_cookies(n-3, cache)
+    return cache[n]
 
 if __name__ == "__main__":
     # Use the main function here to test out your implementation
     num_cookies = 5
 
-    print(f"There are {eating_cookies(num_cookies)} ways for Cookie Monster to each {num_cookies} cookies")
+    print(f"There are {eating_cookies(num_cookies)} ways for Cookie Monster to eat {num_cookies} cookies")
--- single_number/single_number.py
+++ single_number/single_number.py
@@ -2,17 +2,30 @@
 Input: a List of integers where every int except one shows up twice
 Returns: an integer
 '''
-def single_number(arr):
-    # Your code here 
-    dupes = []
-    for i in range(len(arr)): 
-        for x in range(i+1, len(arr)): 
-            if arr[i] == arr[x] and arr[i] not in dupes: 
-                dupes.append(arr[i]) 
-    for num in arr:
-        if num not in dupes:
-            return num 
 
+# def single_number(arr):
+#     # Your code here 
+#     dupes = []
+#     for i in range(len(arr)): 
+#         for x in range(i+1, len(arr)): 
+#             if arr[i] == arr[x] and arr[i] not in dupes: 
+#                 dupes.append(arr[i]) 
+#     for num in arr:
+#         if num not in dupes:
+#             return num 
+
+
+def single_number(arr):
+    counts = {}
+    for num in arr:
+        if num in counts:
+            del counts[num]
+        else:
+            counts[num] = 1
+
+    for k, v in counts.items():
+        if v == 1:
+            return k
 
 if __name__ == '__main__':
     # Use the main function to test your implementation
